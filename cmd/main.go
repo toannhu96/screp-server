330d65aa33a8a76c4407cd928ef2bda406158929
Toan Nhu <toan.nhu@tiki.vn>
2023-02-27 13:39:03 +0700

    mod: update port

--- cmd/main.go
+++ cmd/main.go
@@ -32,7 +32,7 @@ func main() {
 			"status": true,
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":9091")
 }
 
 func process(infile string, outfile string, overview bool) {
